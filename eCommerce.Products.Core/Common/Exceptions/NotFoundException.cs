fe62858da639c450f128a5cd944c45ff4760b715
bamartinezd <alejandromardelg@gmail.com>
2023-08-26 06:07:10 -0500

    deleting garbage to gitignore work properly

--- eCommerce.Products.Core/Common/Exceptions/NotFoundException.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace eCommerce.Products.Core.Common.Exceptions;
-
-public class NotFoundException : Exception
-{
-    public NotFoundException(string? message) : base(message)
-    {
-    }
-}
