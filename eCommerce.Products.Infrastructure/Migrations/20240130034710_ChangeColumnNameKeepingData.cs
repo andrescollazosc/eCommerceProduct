ffb8121a67b7255f32123f6f0df3e3de30a41561
bamartinezd <alejandromardelg@gmail.com>
2024-07-29 08:06:57 -0500

    feat(eCommerceProduct): Add migrations to rename columns

---
+++ eCommerce.Products.Infrastructure/Migrations/20240130034710_ChangeColumnNameKeepingData.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eCommerce.Products.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeColumnNameKeepingData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn("CreatedOn", "Products", "CreatedDate");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn("CreatedDate", "Products", "CreatedOn");
+        }
+    }
+}
